95806878dcef5cdc01b0598e989dbc4acb14c402
Sakshi <sakshisood004@gmail.com>
2025-04-02 17:31:18 +0530

    Add problem statement for customer churn analysis in main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Problem Statement :
+'''
+Customer churn is a significant challenge for financial institutions, as retaining existing customers is often more cost-effective than acquiring new ones. The dataset contains information about bank customers, including demographic details (e.g., age, gender, geography), financial attributes (e.g., credit score, balance, estimated salary), and banking behavior (e.g., tenure, number of products, activity status). However, the bank lacks a clear understanding of the factors driving customer churn and a predictive mechanism to identify customers at risk of leaving. The goal is to analyze this dataset to uncover patterns and build a predictive model that can accurately identify customers likely to churn, enabling the bank to take proactive measures to improve retention.
+'''
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -17,3 +22,4 @@ print(f'Duplicates: {duplicates}\n')
 
 print("Summary Statistics: \n")
 print(data.describe())
+
