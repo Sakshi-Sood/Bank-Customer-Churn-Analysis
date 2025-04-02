4c2cd82795b07622c7d45f7f494a526a27c50df3
Sakshi <sakshisood004@gmail.com>
2025-04-02 17:14:16 +0530

    Enhance initial data exploration in main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 # Load the dataset
-data = pd.read_excel('SalesData.xlsx')
+data = pd.read_excel('Bank_Churn.xlsx')
 
-# Display the first few rows of the dataset
+# Check for  the structure, missing values and data types
 print(data.head())
-
-# Display the columns of the dataset
-print(data.columns)
-
 print(data.info())
-print(data.describe())
 print(data.isnull().sum())
+
+print(data.columns.tolist() , "\n")
+# Check for duplicates
+duplicates = data.duplicated().sum()
+print(f'Duplicates: {duplicates}\n')
+
+print("Summary Statistics: \n")
+print(data.describe())
